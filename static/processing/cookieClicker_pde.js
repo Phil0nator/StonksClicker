543759784ee0dd0a06757fa20df74c94c4d80489
Philo Kaulkin <Phil0nator@users.noreply.github.com>
2020-02-16 23:04:22 -0800

    ++

--- static/processing/cookieClicker_pde.js
+++ static/processing/cookieClicker_pde.js
@@ -4,6 +4,8 @@ var stocks = [];
 var dps = 0; //days per second
 var dpsSlider = document.getElementById("daysPerSecond");
 var lastAutoDay;
+var currentStock = null;
+var daysPassed = 0;
 var stockList =
 [
 
@@ -68,6 +70,14 @@ var stockList =
 function remove_linebreaks( str ) {
     return str.replace( /[\r\n]+/gm, "" );
 }
+
+function truncDisp(s, dgs){
+
+  return remove_linebreaks(s.toString().substring(0, s.toString().indexOf(".") + dgs))
+
+
+}
+
 class Stock{
 
   constructor(name, price){
@@ -77,6 +87,8 @@ class Stock{
     this.ogPrice = price;
     this.owned = 0;
     this.bankrupt = false;
+    this.hist = [];
+    this.hist.push(this.price);
   }
 
   d(x,y){
@@ -85,12 +97,14 @@ class Stock{
     this.y=y;
     var w = width/5;
     var h = height/(12.5);
+    this.w=w;
+    this.h=h;
     var mover = false;
     fill(0,25,0);
     strokeWeight(2);
     stroke(10,255,10);
     if(mouseX>x&&mouseX<x+w&&mouseY>y&&mouseY<y+h){
-
+      currentStock=this;
       textSize(15);
       rect(x+10,y+10,2*(w/3)-20,h-20);
       if(money>this.price){
@@ -124,7 +138,7 @@ class Stock{
     }else{
       dispTrend = "+";
     }
-    dispTrend+=this.trend;
+    dispTrend+=remove_linebreaks(this.trend.toString().substring(0, this.trend.toString().indexOf(".") + 5));;
     dispTrend = remove_linebreaks(dispTrend);
     text(this.name + ": $" + dispPrice + "    >  "+dispTrend, x+width/45,y+height/(stocks.length*3));
 
@@ -135,9 +149,24 @@ class Stock{
   }
 
   tick(){
-
+    if(this.bankrupt){return;}
     this.price-=this.price*this.trend;
-    this.trend+=random(-0.000001,0.000001);
+    this.hist.push(this.price);
+
+    if(this.hist.length>500){
+
+      this.hist.splice(0,1);
+
+    }
+
+    this.trend+=random(-0.00005,0.00005);
+
+    if(this.trend < -0.002){
+      this.trend+=.0003;
+    }else if(this.trend>0.002){
+      this.trend-=.0003;
+    }
+
     if(this.price<1&&!this.bankrupt){
       this.bankrupt=true;
       this.price = 0;
@@ -148,7 +177,12 @@ class Stock{
     if(this.price>this.ogPrice*2){
       this.split();
     }
-
+    //monthly dividend:
+    if(daysPassed%31==0){
+      if(this.trend<0){
+        money-=(this.price*this.trend)*this.owned;
+      }
+    }
   }
 
   split(){
@@ -157,7 +191,59 @@ class Stock{
     this.owned*=2;
   }
 
+  sell(){
+
+    money+=this.price;
+    this.owned--;
+
+  }
+
 }
+
+function plot(stock){
+
+  var sx = width/2;
+  var sy = height/4;
+  var w = width/2;
+  var h = height/2;
+  stroke(10,255,10);
+  fill(1,5,1);
+  strokeWeight(1);
+
+  rect(sx,sy,w,h);
+  if(stock==null){
+    return;
+  }
+  textSize(15);
+  text(stock.name + " -- Price History", sx,sy-15);
+  text("$"+min(stock.hist),sx-textWidth("$"+min(stock.hist))-20,sy+h);
+  text("$"+max(stock.hist),sx-textWidth("$"+max(stock.hist))-20,sy);
+  if(stock.trend<0){
+    stroke(10,255,10);
+    fill(1,5,1);
+  }else{
+    stroke(255,10,10);
+    fill(5,1,1);
+
+  }
+  var xScale = w/stock.hist.length;
+  var yScale = h/(max(stock.hist)-min(stock.hist));
+
+  var graphArr;
+
+  for(var i = 0 ; i < stock.hist.length;i++){
+    if(i>0){
+      line(sx+(i*xScale), (sy+h)-((stock.hist[i]-min(stock.hist))*yScale), sx+((i-1)*xScale), (sy+h)-((stock.hist[i-1]-min(stock.hist))*yScale));
+    }else{
+      point(sx+(i*xScale), (sy+h)-((stock.hist[i]-min(stock.hist))*yScale));
+    }
+
+  }
+
+
+}
+
+
 function loadStocks(){
   for(var i = 0 ; i < stockList.length;i++){
     console.log("Loading: "+stockList[i].name);
@@ -171,7 +257,7 @@ function displayMoney(){
   noStroke();
   fill(10,255,10);
   textSize(text_size);
-  text("$"+money, width/2,height/(text_size/2));
+  text("Balance: $"+truncDisp(money, 4), width/2,height/(text_size/2));
 
 }
 function displayStocks(){
@@ -179,7 +265,6 @@ function displayStocks(){
   for(var i = 0 ; i < stocks.length;i++){
 
     stocks[i].d(50,i*(height/(stocks.length)));
-    console.log("displaying: "+stocks[i].name);
   }
 
 
@@ -193,6 +278,34 @@ function setup() {
   loadStocks();
 }
 
+function sellButton(){
+
+  var w = width/6;
+  var h = height/12;
+  var sx = width/2;
+  var sy = (height*(3/4))+10;
+
+  if(currentStock == null){return;}
+
+  var extLight = 0;
+  if(mouseX>sx&&mouseX<sx+w&&mouseY>sy&&mouseY<sy+h){
+    extLight=25;
+  }
+
+  if(currentStock.owned>0){
+    fill(extLight,extLight+10,extLight);
+    stroke(extLight+10,255,extLight+10)
+
+  }else{
+    fill(extLight+10,0,extLight+10);
+    stroke(255,extLight+10,extLight+10);
+  }
+
+  rect(sx,sy,w,h);
+  text("Sell Shares", sx+20,sy+h/2);
+
+
+}
 
 
 function nextDay(){
@@ -200,13 +313,37 @@ function nextDay(){
   for(var i = 0 ; i < stocks.length;i++){
     stocks[i].tick();
   }
+  daysPassed++;
 
 }
 function mouseReleased(){
-  var r = width/4;
-  if(dist(mouseX,mouseY,width/2,height/2)<r/2){
-   nextDay();
+
+  var w = width/6;
+  var h = height/12;
+  var sx = width/2;
+  var sy = (height*(3/4))+10;
+  if(mouseX>sx&&mouseX<sx+w&&mouseY>sy&&mouseY<sy+h){
+
+    currentStock.sell();
+
   }
+
+
+
+  for(var i =0;i<stocks.length;i++){
+
+    if((mouseX>stocks[i].x&&mouseX<stocks[i].x+stocks[i].w&&mouseY>stocks[i].y&&mouseY<stocks[i].y+stocks[i].h)){
+
+      if(money>stocks[i].price){
+        stocks[i].owned++;
+        money-=stocks[i].price;
+      }
+
+    }
+
+  }
+
+
 }
 
 function nextDayButton(){
@@ -242,7 +379,7 @@ function autoDays(){
   if(millis()-lastAutoDay>1000/dps){
 
     nextDay();
-
+    lastAutoDay=millis();
   }
 
 
@@ -253,8 +390,9 @@ function draw() {
   background(0);
   displayMoney();
   displayStocks();
-  nextDayButton();
+  //nextDayButton();
   labels();
-
+  plot(currentStock);
   autoDays();
+  sellButton();
 }
